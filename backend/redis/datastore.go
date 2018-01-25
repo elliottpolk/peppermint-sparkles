8ed5e31b4aed36989fe914a880f5f4a9bcfcec4f
Elliott Polk <benjamin_elliott_polk@manulifeam.com>
2018-01-25 10:45:49 -0500

    adds redis backend

---
+++ backend/redis/datastore.go
@@ -0,0 +1,98 @@
+// Created by Elliott Polk on 23/01/2018
+// Copyright © 2018 Manulife AM. All rights reserved.
+// oa-montreal/campx/backend/redis/datastore.go
+//
+package redis
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"git.platform.manulife.io/go-common/log"
+
+	"github.com/go-redis/redis"
+	"github.com/pkg/errors"
+)
+
+var ErrInvalidDatastore error = errors.New("no valid datastore")
+
+type Datastore struct {
+	client *redis.Client
+}
+
+func Open(opts *redis.Options) (*Datastore, error) {
+	opts.DB = 0 //	ensure that is uses the same (default) DB every time
+	ds := &Datastore{client: redis.NewClient(opts)}
+
+	//	ensure a valid connection prior to returning the Datastore
+	if _, err := ds.client.Ping().Result(); err != nil {
+		return nil, errors.Wrap(err, "unable to ping redis datastore")
+	}
+
+	return ds, nil
+}
+
+//  Close attempts to close the redis connection of the datastore
+func (ds *Datastore) Close() error {
+	if ds.client != nil {
+		return ds.client.Close()
+	}
+	return nil
+}
+
+//  ToKey takes in a list of strings and hashes using SHA256, returning the
+//  result as a string.
+func (ds *Datastore) ToKey(values ...string) string {
+	in := make([]byte, 0)
+	for _, v := range values {
+		in = append(in, []byte(v)...)
+	}
+	return fmt.Sprintf("%x", sha256.Sum256(in))
+}
+
+func (ds *Datastore) Keys() []string {
+	keys := make([]string, 0)
+	if ds.client != nil {
+		k, err := ds.client.Keys("*").Result()
+		if err != nil {
+			//	log error but still return the empty list
+			log.Error(err, "unable to retrieve keys")
+			return keys
+		}
+
+		for _, v := range k {
+			keys = append(keys, v)
+		}
+	}
+
+	return keys
+}
+
+func (ds *Datastore) Set(key, value string) error {
+	if ds.client == nil {
+		return ErrInvalidDatastore
+	}
+	return ds.client.Set(key, value, 0).Err()
+}
+
+func (ds *Datastore) Get(key string) string {
+	if ds.client != nil {
+		res, err := ds.client.Get(key).Result()
+		if err != nil && err != redis.Nil {
+			//	log error but still return the empty string
+			log.Error(err, "unable to retrieve result for key")
+			return ""
+		}
+		return res
+	}
+
+	return ""
+}
+
+func (ds *Datastore) Remove(key string) error {
+	if ds.client == nil {
+		return ErrInvalidDatastore
+	}
+
+	return ds.client.Del(key).Err()
+}
---
+++ backend/redis/integration_test.go
@@ -0,0 +1,203 @@
+//  +build integration
+
+// Created by Elliott Polk on 23/01/2018
+// Copyright © 2018 Manulife AM. All rights reserved.
+// oa-montreal/campx/backend/redis/integration_test.go
+//
+package redis
+
+import (
+	"fmt"
+	"math/rand"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+func getPort() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		if n := r.Int63n(9999); n > 2000 {
+			return fmt.Sprintf("%d", n)
+		}
+	}
+}
+
+func boot(name, port string) error {
+	n := fmt.Sprintf("--name=%s", name)
+	p := fmt.Sprintf("-p=%s:6379", port)
+
+	return exec.Command("docker", "run", "-d", n, p, "redis:alpine").Run()
+}
+
+func kill(name string) error {
+	if err := exec.Command("docker", "stop", name).Run(); err != nil {
+		return err
+	}
+	return exec.Command("docker", "rm", name).Run()
+}
+
+func TestOpen(t *testing.T) {
+	name := fmt.Sprintf("redis_%d", time.Now().UnixNano())
+	port := getPort()
+	if err := boot(name, port); err != nil {
+		t.Fatal(err)
+	}
+	defer kill(name)
+
+	ds, err := Open(&redis.Options{Addr: fmt.Sprintf("localhost:%s", port)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ds.Close()
+}
+
+func TestClose(t *testing.T) {
+	name := fmt.Sprintf("redis_%d", time.Now().UnixNano())
+	port := getPort()
+	if err := boot(name, port); err != nil {
+		t.Fatal(err)
+	}
+	defer kill(name)
+
+	ds, err := Open(&redis.Options{Addr: fmt.Sprintf("localhost:%s", port)})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ds.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestKeys(t *testing.T) {
+	name := fmt.Sprintf("redis_%d", time.Now().UnixNano())
+	port := getPort()
+	if err := boot(name, port); err != nil {
+		t.Fatal(err)
+	}
+	defer kill(name)
+
+	ds, err := Open(&redis.Options{Addr: fmt.Sprintf("localhost:%s", port)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ds.Close()
+
+	if keys := ds.Keys(); len(keys) > 0 {
+		t.Error("keys should have been empty")
+
+		//	empty, just in case
+		for _, k := range keys {
+			if err := ds.Remove(k); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+
+	wants := []string{"foo", "bar", "baz"}
+	for _, w := range wants {
+		r := rand.NewSource(time.Now().UnixNano()).Int63()
+		if err := ds.Set(ds.ToKey(w), fmt.Sprintf("%d", r)); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	keys := ds.Keys()
+	if got, want := len(keys), len(wants); want != got {
+		t.Errorf("\nwant %d\ngot %d\n", want, got)
+	}
+
+	for _, want := range wants {
+		found := false
+		for _, got := range keys {
+			if ds.ToKey(want) == got {
+				found = true
+			}
+		}
+
+		if !found {
+			t.Error("missing key", want)
+		}
+	}
+}
+
+func TestSetGet(t *testing.T) {
+	name := fmt.Sprintf("redis_%d", time.Now().UnixNano())
+	port := getPort()
+	if err := boot(name, port); err != nil {
+		t.Fatal(err)
+	}
+	defer kill(name)
+
+	ds, err := Open(&redis.Options{Addr: fmt.Sprintf("localhost:%s", port)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ds.Close()
+
+	key, bar := ds.ToKey("foo"), "bar"
+	if got := ds.Get(key); len(got) > 0 {
+		t.Error("expected an empty result but returned", got)
+	}
+
+	if err := ds.Set(key, bar); err != nil {
+		t.Fatal(err)
+	}
+
+	if got := ds.Get(key); bar != got {
+		t.Errorf("\nwant %s\ngot %s\n", bar, got)
+	}
+
+	baz := "baz"
+	if err := ds.Set(key, baz); err != nil {
+		t.Fatal(err)
+	}
+
+	got := ds.Get(key)
+	switch got {
+	case bar:
+		t.Error("value remains the same after an update")
+
+	case baz:
+		break
+
+	default:
+		t.Error("Get returned unknown value of", got)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	name := fmt.Sprintf("redis_%d", time.Now().UnixNano())
+	port := getPort()
+	if err := boot(name, port); err != nil {
+		t.Fatal(err)
+	}
+	defer kill(name)
+
+	ds, err := Open(&redis.Options{Addr: fmt.Sprintf("localhost:%s", port)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ds.Close()
+
+	key, bar := ds.ToKey("foo"), "bar"
+	if err := ds.Set(key, bar); err != nil {
+		t.Fatal(err)
+	}
+
+	//	test if set worked
+	if got := ds.Get(key); bar != got {
+		t.Errorf("\nwant %s\ngot %s\n", bar, got)
+	}
+
+	if err := ds.Remove(key); err != nil {
+		t.Fatal(err)
+	}
+
+	if got := ds.Get(key); len(got) > 0 || got == bar {
+		t.Errorf("non-empty value of %s was returned after removal", got)
+	}
+}
