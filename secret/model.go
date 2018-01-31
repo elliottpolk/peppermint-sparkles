fc0f26c13431b407f476b6b65e7148de6f58f897
Elliott Polk <benjamin_elliott_polk@manulifeam.com>
2018-01-31 08:57:18 -0500

    adds secret model

---
+++ secret/model.go
@@ -0,0 +1,28 @@
+// Created by Elliott Polk on 25/01/2018
+// Copyright © 2018 Manulife AM. All rights reserved.
+// oa-montreal/campx/secret/model.go
+//
+package secret
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
+type Secret struct {
+	Id      string `json:"id,omitempty"`
+	App     string `json:"app_name"`
+	Env     string `json:"env"`
+	Content string `json:"content"`
+}
+
+func NewSecret(raw string) (*Secret, error) {
+	s := &Secret{}
+
+	if err := json.Unmarshal([]byte(raw), &s); err != nil {
+		return nil, errors.Wrap(err, "unable to unmarshal raw secret")
+	}
+
+	return s, nil
+}
