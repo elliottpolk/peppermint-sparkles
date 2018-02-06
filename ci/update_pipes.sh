37485c05ca199cf627732d86e9b9a74d5240d3db
Elliott Polk <benjamin_elliott_polk@manulifeam.com>
2018-02-06 10:46:15 -0500

    cleaning up configs / manifests after refactor

--- ci/update_pipes.sh
+++ ci/update_pipes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-fly -t dev set-pipeline -p secrets-dev -c dev.pipeline.yml -n -l concourse-credentials.yml && \
-    fly -t dev unpause-pipeline -p secrets-dev
+fly -t dev set-pipeline -p peppermint-sparkles -c dev.pipeline.yml -n -l concourse-credentials.yml && \
+    fly -t dev unpause-pipeline -p peppermint-sparkles
