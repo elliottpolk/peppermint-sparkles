2cb2d4562501e62bcedfff730926f39a14531800
Elliott Polk <benjamin_elliott_polk@manulifeam.com>
2018-03-05 14:18:58 -0500

    refactors from secret model to multi-models

---
+++ models/history.go
@@ -0,0 +1,66 @@
+// Copyright © 2018 Manulife AM. All rights reserved.
+// oa-montreal/peppermint-sparkles/models/history.go
+//
+package models
+
+import (
+	"encoding/json"
+
+	"gitlab.manulife.com/oa-montreal/peppermint-sparkles/backend"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	CreateAction  string = "create"
+	UpdateAction  string = "update"
+	ArchiveAction string = "archive"
+)
+
+type Historical struct {
+	*Record `json:"record"`
+
+	Action    string `json:"action"`
+	Created   int64  `json:"created"`
+	CreatedBy string `json:"created_by"`
+}
+
+func FromCurrent(what string) (*Historical, error) {
+	r, err := ParseRecord(what)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse current record")
+	}
+
+	return &Historical{Record: r}, nil
+}
+
+func (h *Historical) Write(where backend.Datastore, why, who string, when int64) error {
+	h.Action = why
+	h.CreatedBy = who
+	h.Created = when
+
+	out, err := h.String()
+	if err != nil {
+		return errors.Wrap(err, "unable to prep historical for storage")
+	}
+
+	return where.AddHistory(out)
+}
+
+func (h *Historical) String() (string, error) {
+	out, err := json.MarshalIndent(h, "", " ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+func (h *Historical) MustString() string {
+	str, err := h.String()
+	if err != nil {
+		return ""
+	}
+
+	return str
+}
